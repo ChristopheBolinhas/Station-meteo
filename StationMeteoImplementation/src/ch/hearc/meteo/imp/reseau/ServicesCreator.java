a2ff490a475dbf264833af288579439871b67822
JuUsT <christophe.bolinhas@he-arc.ch>
2014-06-02 15:41:02 +0200

    utf8
    Mise en place de l'encodage utf8

--- StationMeteoImplementation/src/ch/hearc/meteo/imp/reseau/ServicesCreator.java
+++ StationMeteoImplementation/src/ch/hearc/meteo/imp/reseau/ServicesCreator.java
@@ -1,5 +1,36 @@
 package ch.hearc.meteo.imp.reseau;
 
+import java.rmi.RemoteException;
+
+import ch.hearc.meteo.imp.afficheur.simulateur.AfficheurSimulateurFactory;
+import ch.hearc.meteo.spec.afficheur.AffichageOptions;
+import ch.hearc.meteo.spec.afficheur.AfficheurFactory_I;
+import ch.hearc.meteo.spec.afficheur.AfficheurService_I;
+import ch.hearc.meteo.spec.com.meteo.exception.MeteoServiceException;
+import ch.hearc.meteo.spec.reseau.rmiwrapper.MeteoServiceWrapper_I;
+
 public class ServicesCreator {
+
+	public ServicesCreator() {
+		afficheurFactory = new AfficheurSimulateurFactory();
+	}
+
+	private AfficheurService_I createAfficheurService(
+			final AffichageOptions affichageOptions,
+			final MeteoServiceWrapper_I meteoServiceRemote)
+			throws RemoteException {
+		// AfficheurService_I afficheurService = new AfficheurServiceWrapper()
+		try {
+			meteoServiceRemote.connect();
+		} catch (MeteoServiceException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		return afficheurFactory.createOnCentralPC(affichageOptions,
+				meteoServiceRemote);
+	}
+
+	AfficheurFactory_I afficheurFactory;
 
 }
